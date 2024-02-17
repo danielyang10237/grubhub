db74e8edd49c5deddb67e19240dbcaa9bc1f6985
Eclipse842 <ben.o.friedman@gmail.com>
2024-02-16 22:12:25 -0800

    Add `commitment` and `interests` fields

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,8 +1,10 @@
 use std::fs;
 
+use axum::{routing::get, Router};
 use bb8::ManageConnection;
 use bb8_sqlite::RusqliteConnectionManager;
 use color_eyre::Result;
+use tokio::net::TcpListener;
 use tracing_subscriber::{util::SubscriberInitExt, EnvFilter};
 
 #[tokio::main]
@@ -19,7 +21,7 @@ async fn main2() -> Result<()> {
 
     _ = fs::remove_file("treehacks.db");
 
-    tracing::error!("hi!");
+    tracing::trace!("hi!");
 
     let connection_manager = RusqliteConnectionManager::new("treehacks.db");
 
@@ -28,6 +30,10 @@ async fn main2() -> Result<()> {
     conn.pragma_update(None, "FOREIGN_KEY", "ON")?;
     conn.execute_batch(CREATE_TABLES)?;
 
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let listener = TcpListener::bind("0.0.0.0:3000").await?;
+    axum::serve(listener, app).await?;
     Ok(())
 }
 
@@ -39,7 +45,8 @@ CREATE TABLE users (
 
 CREATE TABLE groups (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL
+    name TEXT NOT NULL,
+    commitment INTEGER
 );
 
 CREATE TABLE events (
@@ -49,14 +56,26 @@ CREATE TABLE events (
     end_time TEXT NOT NULL
 );
 
+CREATE TABLE interests (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL
+);
+
 CREATE TABLE group_members (
     group_id REFERENCES groups(id),
-    user_id REFERENCES users(id)
+    user_id REFERENCES users(id),
+    commitment INTEGER
 );
 
 CREATE TABLE group_events (
     group_id REFERENCES groups(id),
     event_id REFERENCES events(id)
+);
+
+CREATE TABLE user_interests (
+    user_id REFERENCES users(id),
+    interest_id REFERENCES interests(id),
+    score INTEGER
 );
 
 CREATE TABLE user_events (
