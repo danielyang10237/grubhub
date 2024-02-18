54da6a2a0fe0880b1a90b53769996df9896c9f07
bkamm <70417491+bkamm@users.noreply.github.com>
2024-02-18 00:48:21 -0800

    Finished API helpers.

--- frontend/src/pages/directory.tsx
+++ frontend/src/pages/directory.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { getAllGroups } from './api/hello';
+import { getAllGroups, findGroups, getAllTags, getGroup } from './api/helpers';
 import { Group, GroupSearch, GroupSearchEntry, GroupSearchOptions } from "./api/interfaces"
 
-// Your Directory component
+// component for Directory page
 const Directory = ({ groups }) => {
   return (
     <>
       <h1 className="text-4xl font-bold">This is the directory page</h1>
+      <h1 className="text-4xl font-bold">List of all groups:</h1>
       <ul>
         {groups.map(group => (
           <li key={group.id}>{group.name}</li>
