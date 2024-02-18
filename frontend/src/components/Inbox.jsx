edce8e292f9eda8ace4192320b5c7bb7d0d6fb06
Daniel Yang <danielyang@DN0a1f0f91.SUNet>
2024-02-18 08:24:39 -0800

    yaaaaaaa

--- frontend/src/components/Inbox.jsx
+++ frontend/src/components/Inbox.jsx
@@ -5,17 +5,18 @@ import "../css/inbox.css";
 
 const Inbox = () => {
   const [mails, setMails] = useState([]);
-  const [selectedMail, setSelectedMail] = useState(null);
+  const [selectedMail, setSelectedMail] = useState(0);
 
   const setCurrentMail = (mail) => {
     setSelectedMail(mail);
+    console.log(mail);
   };
 
   useEffect(() => {
     axios
       .get("http://localhost:3001/users/0/inbox")
       .then((response) => {
-        // setMails(response.data);
+        setMails(response.data);
       })
       .catch((error) => {
         console.log(error);
@@ -30,20 +31,33 @@ const Inbox = () => {
         <div className="mail-container">
           <div className="mails">
             {mails.map((mail, index) => (
-              <div key={index} className={`mail`} onClick={()=>{setCurrentMail(mail.announcement)}}>
-                {mail.viewed ? null : <div className={"read-bubble"}></div>}
-                <div className="mail-tag">
-                  <p className="mail-sender">{mail.sender}</p>
-                  <p className="mail-time">{mail.time}</p>
-                  <p className="mail-title">{mail.title}</p>
-                </div>
+              <div
+                key={index}
+                className={`mail ${
+                  mail.announcement === selectedMail
+                 ? "selected-mail" : ""}`}
+                onClick={() => {
+                  setCurrentMail(mail.announcement);
+                }}
+              >
+                <p className="mail-sender">
+                  {mail.viewed ? <div></div> : <div className={"read-bubble"}></div>}
+                  {mail.sender}
+                </p>
+                <p className="mail-time">{mail.time}</p>
               </div>
             ))}
           </div>
           <div className="mail-content">
             {mails.map((mail, index) => (
-              <div key={index} className={`mail ${mail.announcement === selectedMail} ? "" : "hidden"`}>
-                <h3>{mail.title}</h3>
+              <div
+                key={index}
+                className={`message ${
+                  mail.announcement === selectedMail
+                 ? "" : "hidden"}`}
+              >
+                <h3 className="subject-line">Subject: {mail.subject}</h3>
+                <h4 className="mail-words">{mail.body}</h4>
               </div>
             ))}
           </div>
